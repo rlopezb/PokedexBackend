2258ec0841b75da1a93d7da13534cbefc3ae0263
Rafael López Benavente <rafael.lopez@vodafone.com>
2023-06-13 13:57:19 +0200

    Cross and default pagination

--- src/main/java/es/pokemon/pokedex/controller/PomemonController.java
+++ src/main/java/es/pokemon/pokedex/controller/PomemonController.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.SortDefault;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,8 @@ public class PomemonController {
   PokemonService pokemonService;
 
   @GetMapping
-  Page<Pokemon> findAll(@SortDefault(sort = "id", direction = Sort.Direction.ASC) final Pageable pageable){
+  @CrossOrigin(origins = "http://localhost:3000")
+  Page<Pokemon> findAll(@SortDefault(sort = "id", direction = Sort.Direction.ASC) final Pageable pageable) {
     return pokemonService.findAll(pageable);
   }
 }
